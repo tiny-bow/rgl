d60cb3ba00708bcb6791ee1363db575f2a2ab784
noxabellus <noxabellus@gmail.com>
2025-04-23 15:43:53 -0500

    consistent api naming scheme across tiny-bow forks

--- build.zig
+++ build.zig
@@ -4,8 +4,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zgl", .{
-        .root_source_file = b.path("src/zgl.zig"),
+    _ = b.addModule("rgl", .{
+        .root_source_file = b.path("src/rgl.zig"),
         .target = target,
         .optimize = optimize,
     });
